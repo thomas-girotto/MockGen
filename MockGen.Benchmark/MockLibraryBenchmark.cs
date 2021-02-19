f3a47c143867f1e3892256a35db13d24f0def58f
thomas-girotto <thomas.girotto@gmail.com>
2021-02-19 11:46:58 +0100

    chore(benchmark): add Moq lib to benchmark

--- MockGen.Benchmark/MockLibraryBenchmark.cs
+++ MockGen.Benchmark/MockLibraryBenchmark.cs
@@ -8,8 +8,8 @@ namespace MockGen.Benchmark
 {
     public class MockLibraryBenchmark
     {
-        [Benchmark]
-        public void MockGenMock()
+        [Benchmark(Baseline = true, Description = "MockGen")]
+        public void MockGenBench()
         {
             var mockGen = Mock<IDependency>.Create();
             mockGen.GetSomeNumber().WillReturn(2);
@@ -18,14 +18,24 @@ namespace MockGen.Benchmark
             mockGen.GetSomeNumber().Calls.Should().Be(1);
         }
 
-        [Benchmark]
-        public void NSubstitute()
+        [Benchmark(Description = "NSubstitute")]
+        public void NSubstituteBench()
         {
             var substitute = Substitute.For<IDependency>();
             substitute.GetSomeNumber().Returns(2);
             var service = new Service(substitute);
             service.ReturnDependencyNumber();
             substitute.Received(1).GetSomeNumber();
+        }
+
+        [Benchmark(Description = "Moq")]
+        public void MoqBench()
+        {
+            var moq = new Moq.Mock<IDependency>();
+            moq.Setup(d => d.GetSomeNumber()).Returns(2);
+            var service = new Service(moq.Object);
+            service.ReturnDependencyNumber();
+            moq.Verify(d => d.GetSomeNumber(), Moq.Times.Once);
         }
     }
 }
