79b665225f6db7fe5e1fa464a752a6eeed240094
thomas-girotto <thomas.girotto@gmail.com>
2021-02-27 04:26:10 +0100

    tests: found a way to unit test the syntax receiver

---
+++ MockGen.Tests/SyntaxReceiverTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp;
+using MockGen.Tests.Utils;
+using Xunit;
+
+namespace MockGen.Tests
+{
+    public class SyntaxReceiverTests
+    {
+        [Fact]
+        public void Should_extract_the_type_to_mock()
+        {
+            // Given
+            var source = "MockGenerator.Generate<IDependency>();";
+            var rootNode = CSharpSyntaxTree.ParseText(source).GetRoot();
+            var syntaxReceiver = new SyntaxReceiver();
+            var syntaxTreeVisitor = new MockGeneratorSyntaxWalker(syntaxReceiver);
+
+            // When
+            syntaxTreeVisitor.Visit(rootNode);
+
+            // Then
+            syntaxReceiver.TypesToMockSyntax.Should().ContainSingle().Which.Identifier.ValueText.Should().Be("IDependency");
+        }
+
+        [Fact]
+        public void Should_only_add_same_type_to_mock_once()
+        {
+            // Given
+            var source = @"
+MockGenerator.Generate<IDependency>();
+MockGenerator.Generate<IDependency>();
+";
+            var rootNode = CSharpSyntaxTree.ParseText(source).GetRoot();
+            var syntaxReceiver = new SyntaxReceiver();
+            var syntaxTreeVisitor = new MockGeneratorSyntaxWalker(syntaxReceiver);
+
+            // When
+            syntaxTreeVisitor.Visit(rootNode);
+
+            // Then
+            syntaxReceiver.TypesToMockSyntax.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Should_add_several_types_When_several_declarations()
+        {
+            // Given
+            var source = @"
+MockGenerator.Generate<IDependency1>();
+MockGenerator.Generate<IDependency2>();
+";
+            var rootNode = CSharpSyntaxTree.ParseText(source).GetRoot();
+            var syntaxReceiver = new SyntaxReceiver();
+            var syntaxTreeVisitor = new MockGeneratorSyntaxWalker(syntaxReceiver);
+
+            // When
+            syntaxTreeVisitor.Visit(rootNode);
+
+            // Then
+            syntaxReceiver.TypesToMockSyntax.Should().HaveCount(2);
+        }
+
+        [Theory]
+        [InlineData("MockGenerator.Generate<IDependency>")]
+        [InlineData("MockGenerator.Generate<IDependency>(1)")]
+        [InlineData("MockGenerator.WrongName<IDependency>()")]
+        [InlineData("WrongClass.Generate<IDependency>()")]
+        public void Should_not_find_type_to_mock_When_syntax_is_wrong(string source)
+        {
+            // Given
+            var rootNode = CSharpSyntaxTree.ParseText(source).GetRoot();
+            var syntaxReceiver = new SyntaxReceiver();
+            var syntaxTreeVisitor = new MockGeneratorSyntaxWalker(syntaxReceiver);
+
+            // When
+            syntaxTreeVisitor.Visit(rootNode);
+
+            // Then
+            syntaxReceiver.TypesToMockSyntax.Should().BeEmpty();
+        }
+    }
+}
---
+++ MockGen.Tests/Utils/MockGeneratorSyntaxWalker.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace MockGen.Tests.Utils
+{
+    /// <summary>
+    /// Helps visiting the code syntax tree from the given node as root.
+    /// </summary>
+    public class MockGeneratorSyntaxWalker : CSharpSyntaxWalker
+    {
+        private readonly SyntaxReceiver syntaxReceiver;
+
+        public MockGeneratorSyntaxWalker(SyntaxReceiver syntaxReceiver)
+        {
+            this.syntaxReceiver = syntaxReceiver;
+        }
+
+        public override void Visit(SyntaxNode node)
+        {
+            // call our syntax receiver so that we can test it
+            syntaxReceiver.OnVisitSyntaxNode(node);
+            base.Visit(node);
+        }
+    }
+}
