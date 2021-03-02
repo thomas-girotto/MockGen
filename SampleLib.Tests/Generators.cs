d20074b5bdc565ba9068b6d35d09cbae5947a583
thomas-girotto <thomas.girotto@gmail.com>
2021-03-03 00:27:09 +0100

    fix(sample): update sample to work with latest MockGen

---
+++ SampleLib.Tests/Generators.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MockGen;
+
+namespace SampleLib.Tests
+{
+    public class Generators
+    {
+        public void GenerateMocks()
+        {
+            MockGenerator.Generate<IExternalDependency>();
+        }
+    }
+}
--- SampleLib.Tests/ServiceTests.cs
+++ SampleLib.Tests/ServiceTests.cs
@@ -1,6 +1,5 @@
 using Xunit;
 using MockGen;
-using MockGen.IExternalDependencyNs;
 using FluentAssertions;
 
 namespace SampleLib.Tests
@@ -10,28 +9,28 @@ namespace SampleLib.Tests
         [Fact]
         public void Should_add_number_from_external_source_to_given_number()
         {
-            var mock = Mock<IExternalDependency>.Create();
+            var mock = Mock.IExternalDependency();
             mock.GetSomeNumber().Returns(2);
 
             var service = new Service(mock.Build());
             var number = service.ReturnDependencyNumber();
 
             number.Should().Be(2);
-            mock.GetSomeNumber().Calls.Should().Be(1);
+            mock.GetSomeNumber().NumberOfCalls.Should().Be(1);
         }
 
         [Fact]
         public void MethodVoidWithReferenceTypeParam_Should_handle_null_values()
         {
-            var mock = Mock<IExternalDependency>.Create();
+            var mock = Mock.IExternalDependency();
             var model = new Model { Id = 1 };
             var service = new Service(mock.Build());
             service.DoSomething(model);
             service.DoSomething(null);
 
-            mock.DoSomething(Arg<Model>.Any).Calls.Should().Be(2);
-            mock.DoSomething(null).Calls.Should().Be(1);
-            mock.DoSomething(model).Calls.Should().Be(1);
+            mock.DoSomething(Arg<Model>.Any).NumberOfCalls.Should().Be(2);
+            mock.DoSomething(null).NumberOfCalls.Should().Be(1);
+            mock.DoSomething(model).NumberOfCalls.Should().Be(1);
         }
     }
 }
