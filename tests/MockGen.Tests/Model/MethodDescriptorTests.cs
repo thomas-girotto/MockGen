e7f863c9ea541ddb09c2687d38c3f326739b1c98
thomas-girotto <thomas.girotto@gmail.com>
2021-04-02 00:08:32 +0200

    refactor: one line assertion is more readable, plus rename test methods

--- tests/MockGen.Tests/Model/MethodDescriptorTests.cs
+++ tests/MockGen.Tests/Model/MethodDescriptorTests.cs
@@ -8,56 +8,58 @@ namespace MockGen.Tests.Model
 {
     public class MethodDescriptorTests
     {
-        private MethodDescriptor GetMethodVoid() => new MethodDescriptor
+        private MethodDescriptor MethodVoid => new MethodDescriptor
         {
             Name = "MethodVoid",
             ReturnType = ReturnType.Void,
         };
 
-        private MethodDescriptor GetMethodVoidWithParam() => new MethodDescriptor
+        private MethodDescriptor MethodVoidWithParam => new MethodDescriptor
         {
             Name = "MethodVoidWithParam",
             ReturnType = ReturnType.Void,
-            Parameters = new List<ParameterDescriptor> { new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1") },
+            Parameters = new List<ParameterDescriptor> { new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1", false) },
         };
 
-        private MethodDescriptor GetMethodVoidWithTwoParam() => new MethodDescriptor
+        private MethodDescriptor MethodVoidWithTwoParam => new MethodDescriptor
         {
             Name = "MethodVoidWithTwoParam",
             ReturnType = ReturnType.Void,
-            Parameters = new List<ParameterDescriptor> 
-            { 
-                new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1"),
-                new ParameterDescriptor(new Type("Type2", "SomeLib.Namespace"), "param2"),
+            Parameters = new List<ParameterDescriptor>
+            {
+                new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1", false),
+                new ParameterDescriptor(new Type("Type2", "SomeLib.Namespace"), "param2", false),
             },
         };
 
-        private MethodDescriptor GetMethodWithoutParameter() => new MethodDescriptor
+        private MethodDescriptor MethodThatReturnsWithoutParameter => new MethodDescriptor
         {
             Name = "Method1",
             ReturnType = new ReturnType("int", false),
         };
 
-        private MethodDescriptor GetMethodWithOneParameter() => new MethodDescriptor
+        private MethodDescriptor MethodThatReturnsWithOneParameter => new MethodDescriptor
         {
             Name = "Method2",
             ReturnType = new ReturnType("int", false),
-            Parameters = new List<ParameterDescriptor> { new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1") }
+            Parameters = new List<ParameterDescriptor> { new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1", false) }
         };
 
-        private MethodDescriptor GetMethodWithTwoParameters() => new MethodDescriptor
+        private MethodDescriptor MethodThatReturnsWithTwoParameters => new MethodDescriptor
         {
             Name = "Method3",
             ReturnType = new ReturnType("int", false),
-            Parameters = new List<ParameterDescriptor> 
-            { 
-                new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1"), 
-                new ParameterDescriptor(new Type("Type2", "SomeLib.Namespace"), "param2"),
+            Parameters = new List<ParameterDescriptor>
+            {
+                new ParameterDescriptor(new Type("Type1", "SomeLib.Namespace"), "param1", false),
+                new ParameterDescriptor(new Type("Type2", "SomeLib.Namespace"), "param2", false),
             }
         };
 
+
+
         [Fact]
-        public void Should_set_a_unique_name_for_methods_overload()
+        public void AddMethod_Should_set_a_unique_name_for_methods_overload()
         {
             var mock = new MockDescriptor();
             var method1 = new MethodDescriptor { Name = "DoSomething" };
@@ -75,91 +77,57 @@ namespace MockGen.Tests.Model
         }
 
         [Fact]
-        public void Should_build_MethodSetup_with_generic_typed_parameters()
+        public void MethodSetupWithTypedParameters_Should_build_MethodSetup_with_generic_typed_parameters()
         {
-            string methodSetupVoid = GetMethodVoid().MethodSetupWithTypedParameters;
-            string methodSetupVoidWithParam = GetMethodVoidWithParam().MethodSetupWithTypedParameters;
-            string methodSetupTReturn = GetMethodWithoutParameter().MethodSetupWithTypedParameters;
-            string methodSetupTReturnTParam = GetMethodWithOneParameter().MethodSetupWithTypedParameters;
-            string methodSetupTReturnTParam1TParam2 = GetMethodWithTwoParameters().MethodSetupWithTypedParameters;
-            
-            // Assert
-            methodSetupVoid.Should().Be("MethodSetupVoid");
-            methodSetupVoidWithParam.Should().Be("MethodSetupVoid<Type1>");
-            methodSetupTReturn.Should().Be("MethodSetupReturn<int>");
-            methodSetupTReturnTParam.Should().Be("MethodSetupReturn<Type1, int>");
-            methodSetupTReturnTParam1TParam2.Should().Be("MethodSetupReturn<Type1, Type2, int>");
+            MethodVoid.MethodSetupWithTypedParameters.Should().Be("MethodSetupVoid");
+            MethodVoidWithParam.MethodSetupWithTypedParameters.Should().Be("MethodSetupVoid<Type1>");
+            MethodVoidWithTwoParam.MethodSetupWithTypedParameters.Should().Be("MethodSetupVoid<Type1, Type2>");
+            MethodThatReturnsWithoutParameter.MethodSetupWithTypedParameters.Should().Be("MethodSetupReturn<int>");
+            MethodThatReturnsWithOneParameter.MethodSetupWithTypedParameters.Should().Be("MethodSetupReturn<Type1, int>");
+            MethodThatReturnsWithTwoParameters.MethodSetupWithTypedParameters.Should().Be("MethodSetupReturn<Type1, Type2, int>");
         }
 
         [Fact]
-        public void Should_build_IMethodSetup_with_generic_typed_parameters()
+        public void IMethodSetupWithTypedParameters_Should_build_IMethodSetup_with_generic_typed_parameters()
         {
-            string methodSetupVoid = GetMethodVoid().IMethodSetupWithTypedParameters;
-            string methodSetupVoidWithParam = GetMethodVoidWithParam().IMethodSetupWithTypedParameters;
-            string methodSetupVoidWithTwoParam = GetMethodVoidWithTwoParam().IMethodSetupWithTypedParameters;
-            string methodSetupTReturn = GetMethodWithoutParameter().IMethodSetupWithTypedParameters;
-            string methodSetupTReturnTParam = GetMethodWithOneParameter().IMethodSetupWithTypedParameters;
-            string methodSetupTReturnTParam1TParam2 = GetMethodWithTwoParameters().IMethodSetupWithTypedParameters;
-
-            // Assert
-            methodSetupVoid.Should().Be("IMethodSetup");
-            methodSetupVoidWithParam.Should().Be("IMethodSetup<Type1>");
-            methodSetupVoidWithTwoParam.Should().Be("IMethodSetup<Type1, Type2>");
-            methodSetupTReturn.Should().Be("IMethodSetupReturn<int>");
-            methodSetupTReturnTParam.Should().Be("IMethodSetupReturn<Type1, int>");
-            methodSetupTReturnTParam1TParam2.Should().Be("IMethodSetupReturn<Type1, Type2, int>");
+            MethodVoid.IMethodSetupWithTypedParameters.Should().Be("IMethodSetup");
+            MethodVoidWithParam.IMethodSetupWithTypedParameters.Should().Be("IMethodSetup<Type1>");
+            MethodVoidWithTwoParam.IMethodSetupWithTypedParameters.Should().Be("IMethodSetup<Type1, Type2>");
+            MethodThatReturnsWithoutParameter.IMethodSetupWithTypedParameters.Should().Be("IMethodSetupReturn<int>");
+            MethodThatReturnsWithOneParameter.IMethodSetupWithTypedParameters.Should().Be("IMethodSetupReturn<Type1, int>");
+            MethodThatReturnsWithTwoParameters.IMethodSetupWithTypedParameters.Should().Be("IMethodSetupReturn<Type1, Type2, int>");
         }
 
         [Fact]
-        public void Should_build_method_parameter_declaration()
+        public void ParametersDeclaration_Should_concat_parameters_type_and_names()
         {
-            string parametersDefinition1 = GetMethodWithoutParameter().ParametersDeclaration;
-            string parametersDefinition2 = GetMethodWithOneParameter().ParametersDeclaration;
-            string parametersDefinition3 = GetMethodWithTwoParameters().ParametersDeclaration;
-            var test = string.Join(", ", "methods", null);
-            // Assert
-            parametersDefinition1.Should().BeEmpty();
-            parametersDefinition2.Should().Be("Type1 param1");
-            parametersDefinition3.Should().Be("Type1 param1, Type2 param2");
+            MethodThatReturnsWithoutParameter.ParametersDeclaration.Should().BeEmpty();
+            MethodThatReturnsWithOneParameter.ParametersDeclaration.Should().Be("Type1 param1");
+            MethodThatReturnsWithTwoParameters.ParametersDeclaration.Should().Be("Type1 param1, Type2 param2");
         }
 
         [Fact]
-        public void Should_build_method_parameter_declaration_with_Arg()
+        public void ParametersDeclarationWithArg_Should_build_method_parameter_declaration_with_Arg()
         {
-            string parametersDefinition1 = GetMethodWithoutParameter().ParametersDeclarationWithArg;
-            string parametersDefinition2 = GetMethodWithOneParameter().ParametersDeclarationWithArg;
-            string parametersDefinition3 = GetMethodWithTwoParameters().ParametersDeclarationWithArg;
-
-            // Assert
-            parametersDefinition1.Should().BeEmpty();
-            parametersDefinition2.Should().Be("Arg<Type1> param1");
-            parametersDefinition3.Should().Be("Arg<Type1> param1, Arg<Type2> param2");
+            MethodThatReturnsWithoutParameter.ParametersDeclarationWithArg.Should().BeEmpty();
+            MethodThatReturnsWithOneParameter.ParametersDeclarationWithArg.Should().Be("Arg<Type1> param1");
+            MethodThatReturnsWithTwoParameters.ParametersDeclarationWithArg.Should().Be("Arg<Type1> param1, Arg<Type2> param2");
         }
 
         [Fact]
-        public void Should_build_method_parameters()
+        public void ParameterNames_Should_concat_names_of_parameters()
         {
-            string parameters1 = GetMethodWithoutParameter().ParameterNames;
-            string parameters2 = GetMethodWithOneParameter().ParameterNames;
-            string parameters3 = GetMethodWithTwoParameters().ParameterNames;
-
-            // Assert
-            parameters1.Should().BeEmpty();
-            parameters2.Should().Be("param1");
-            parameters3.Should().Be("param1, param2");
+            MethodThatReturnsWithoutParameter.ParameterNames.Should().BeEmpty();
+            MethodThatReturnsWithOneParameter.ParameterNames.Should().Be("param1");
+            MethodThatReturnsWithTwoParameters.ParameterNames.Should().Be("param1, param2");
         }
 
         [Fact]
-        public void Should_add_ForParameter_method_When_parameter_exists()
+        public void CallForParameterMethod_Should_concat_parameters_with_null_check()
         {
-            string forParameterCall1 = GetMethodWithoutParameter().CallForParameterMethod;
-            string forParameterCall2 = GetMethodWithOneParameter().CallForParameterMethod;
-            string forParameterCall3 = GetMethodWithTwoParameters().CallForParameterMethod;
-
-            // Assert
-            forParameterCall1.Should().BeEmpty();
-            forParameterCall2.Should().Be("param1 ?? Arg<Type1>.Null");
-            forParameterCall3.Should().Be("param1 ?? Arg<Type1>.Null, param2 ?? Arg<Type2>.Null");
+            MethodThatReturnsWithoutParameter.CallForParameterMethod.Should().BeEmpty();
+            MethodThatReturnsWithOneParameter.CallForParameterMethod.Should().Be("param1 ?? Arg<Type1>.Null");
+            MethodThatReturnsWithTwoParameters.CallForParameterMethod.Should().Be("param1 ?? Arg<Type1>.Null, param2 ?? Arg<Type2>.Null");
         }
     }
 }
